6663661bfb783b4fffc8e71083f9d0ee6a41c523
son <handsun000@gmail.com>
2024-12-11 12:35:07 +0900

    ApplicationRunner

--- src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
@@ -1,11 +1,15 @@
 package com.ll.jpa.domain.post.post.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Post {
     //long => null 안됨
     //Long => null 가능
