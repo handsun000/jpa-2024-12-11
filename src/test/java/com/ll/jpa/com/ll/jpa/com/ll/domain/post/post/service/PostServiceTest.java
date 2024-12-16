e077e6973afbe1e917d8de76f932eaec0008da38
son <handsun000@gmail.com>
2024-12-16 11:18:28 +0900

    t1

--- src/test/java/com/ll/jpa/com/ll/jpa/com/ll/domain/post/post/service/PostServiceTest.java
+++ src/test/java/com/ll/jpa/com/ll/jpa/com/ll/domain/post/post/service/PostServiceTest.java
@@ -1,24 +1,25 @@
-package com.ll.domain.post.post.service;
+package com.ll.jpa.com.ll.jpa.com.ll.domain.post.post.service;
 
-import com.ll.jpa.domain.post.post.entity.Post;
 import com.ll.jpa.domain.post.post.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class PostServiceTest {
-
     @Autowired
     private PostService postService;
 
     @Test
     @DisplayName("글 2개 생성")
-    public void t1() {
+    @Transactional
+    @Rollback
+    void t1() {
         postService.write("제목1", "내용1");
         postService.write("제목2", "내용2");
     }
